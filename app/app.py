60c9d6e1a33e7319a9d88dbaa91fbed4c05701a7
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2022-04-30 16:23:01 -0300

    Counting by size and initial letter

--- app/app.py
+++ app/app.py
@@ -34,13 +34,33 @@ words = lines.flatMap(lambda line: line.split(" ")).filter(lambda word: word != 
 pairs = words.map(lambda word: (word.lower(), 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
 
+
+windowWordCount = wordCounts.window(3 ,3)
+
+wordsByWindowWithS = windowWordCount.filter(lambda x: x[0][0] == "s").reduce(lambda x, y: ("Words with S", x[1] + y[1]))
+wordsByWindowWithP = windowWordCount.filter(lambda x: x[0][0] == "p").reduce(lambda x, y: ("Words with P", x[1] + y[1]))
+wordsByWindowWithR = windowWordCount.filter(lambda x: x[0][0] == "r").reduce(lambda x, y: ("Words with R", x[1] + y[1]))
+
+wordsByWindowWithSize6  = windowWordCount.filter(lambda x: len(x[0]) == 6 ).reduce(lambda x, y: ("Words with 6 characters", x[1] + y[1]))
+wordsByWindowWithSize8  = windowWordCount.filter(lambda x: len(x[0]) == 8 ).reduce(lambda x, y: ("Words with 8 characters", x[1] + y[1]))
+wordsByWindowWithSize11 = windowWordCount.filter(lambda x: len(x[0]) == 11 ).reduce(lambda x, y: ("Words with 11 characters", x[1] + y[1]))
+
+
 running = wordCounts.updateStateByKey(updateFunc, initialRDD=state_rdd)
 
 sorted = running.transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
+
 wordsLen = running.reduce(lambda x, y: ("Total number of words", x[1] + y[1]))
 
-sorted.pprint()
-wordsLen.pprint()
+
+##sorted.pprint()
+##wordsLen.pprint()
+##wordsByWindowWithS.pprint()
+##wordsByWindowWithP.pprint()
+##wordsByWindowWithR.pprint()
+wordsByWindowWithSize6.pprint() 
+wordsByWindowWithSize8.pprint() 
+wordsByWindowWithSize11.pprint() 
 
 ssc.start()  # Start the computation
 ssc.awaitTermination()  # Wait for the computation to terminate
