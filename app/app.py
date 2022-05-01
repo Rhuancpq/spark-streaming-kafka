e2bfa7d4a5071b4269331a9324822be3e9f79067
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-05-01 00:42:06 -0300

    Fix app interface with input stream

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-
+import string
 
 conf = (
     SparkConf()
@@ -30,12 +30,13 @@ def updateFunc(new_values, last_sum):
     return sum(new_values) + (last_sum or 0)
 
 
-lines = KafkaUtils.createStream(
+tuples = KafkaUtils.createStream(
     ssc, "zookeeper:2181", "spark-streaming-consumer", {"data-topic": 1}
 )
 
-# FIXME interface changes
-words = lines.flatMap(lambda line: line.split(" ")).filter(lambda word: word != "")
+words = tuples.flatMap(
+    lambda line: line[1].translate(str.maketrans("", "", string.punctuation)).split(" ")
+).filter(lambda word: word != "")
 
 pairs = words.map(lambda word: (word.lower(), 1))
 wordCounts = pairs.reduceByKey(lambda x, y: x + y)
